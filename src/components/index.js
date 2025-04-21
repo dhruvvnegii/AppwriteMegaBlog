e6c4aaaccbff28fd85e224e8093215b4e54e8451
Dhruv <honey.dhruv55@gmail.com>
2025-04-22 01:14:57 +0530

    done

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
